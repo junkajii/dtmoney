1c46878d10c18486cd28382266cbdd87e002f912
Jun Kajii <junk4jii@gmail.com>
2022-04-16 18:37:05 -0300

    :sparkles: add useTransactions hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal'
 import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal'
 import { Dashboard } from './components/Dashboard'
-import { TransactionsProvider } from './TransactionsContext'
+import { TransactionsProvider } from './hooks/useTransactions'
 
 import { GlobalStyle } from './styles/global'
 
